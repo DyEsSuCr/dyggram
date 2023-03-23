70b6642bccf25c0d4887bc53a0b2422bbdcb5a39
DylanSuarez <2000sajo@gmail.com>
2023-03-23 13:53:47 -0500

    sqlite3

--- server/database/db.js
+++ server/database/db.js
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize'
 import { env } from '../config.js'
 
 export const conectionSequelize = new Sequelize(env.DB, env.USER, env.PWD, {
+  dialect: 'sqlite',
   database: env.DB,
-  dialect: 'mysql'
+  host: './db.sqlite3'
 })
