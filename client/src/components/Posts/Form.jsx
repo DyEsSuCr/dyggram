7f3b9607d228c20fbd5e47fa6daad518060d08b7
DylanSuarez <2000sajo@gmail.com>
2023-03-27 21:10:23 -0500

    preview image 1v

---
+++ client/src/components/Posts/Form.jsx
@@ -0,0 +1,46 @@
+import { useForm } from 'react-hook-form'
+import { useState, useEffect } from 'react'
+import { BASE_URL } from '../../services/settings'
+
+export function Form () {
+  const { register, handleSubmit, watch } = useForm()
+  const [cover, setCover] = useState(null)
+
+  const createPost = async (data) => {
+    const { plot, photo } = data
+
+    const formData = new FormData()
+
+    formData.append('photo', photo[0])
+    formData.append('plot', plot)
+
+    fetch(`${BASE_URL}`)
+  }
+
+  const photo = watch('photo')
+
+  useEffect(() => {
+    const reader = new window.FileReader()
+
+    if (!photo) return
+
+    reader.readAsDataURL(photo[0])
+
+    reader.onloadend = function () {
+      setCover(reader.result.toString())
+    }
+  }, [photo])
+
+  return (
+    <form onSubmit={handleSubmit(createPost)}>
+
+      <input type='file' {...register('photo', { required: true })} />
+      {cover && <img src={cover} alt='preview' />}
+
+      <input type='text' {...register('plot', { required: true })} />
+
+      <input type='submit' />
+
+    </form>
+  )
+}
--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,7 +1,10 @@
+import { Form } from '../components/Posts/Form'
+
 export function Create () {
   return (
     <div>
       <h1>Create</h1>
+      <Form />
     </div>
   )
 }
