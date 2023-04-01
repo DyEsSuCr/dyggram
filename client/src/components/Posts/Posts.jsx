1a296083d2130a574c0674b54d8e41137c0db8d9
DylanSuarez <2000sajo@gmail.com>
2023-04-01 15:33:21 -0500

    jsx: ModalPostDetail

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react'
+import { ModalPostDetail } from '../../portals/ModalPostDetail'
+import { createPortal } from 'react-dom'
+import { BASE_URL } from '../../services/settings'
 
 export function Posts ({ userPosts }) {
+  const [post, setPost] = useState(null)
+
+  const infoPost = async (id) => {
+    const datos = await (await fetch(`${BASE_URL}/posts/${id}`)).json()
+    setPost(datos)
+  }
+
   return (
-    <div>
+    <div className='grid grid-cols-3 max-w-7xl'>
       {userPosts?.map(post => (
-        <div key={post.id}>
-          <img className='max-w-sm' src={`http://localhost:3000/images/${post.photo}`} />
-          <p dangerouslySetInnerHTML={{ __html: post.plot }} />
+        <div className='mx-auto' key={post.id}>
+          <button onClick={() => infoPost(post.id)}>
+            <img className='max-w-sm' src={`http://localhost:3000/images/${post.photo}`} />
+          </button>
         </div>
       ))}
+      {post && createPortal(<ModalPostDetail post={post} setPost={setPost} />, document.getElementById('post-detail'))}
     </div>
   )
 }
---
+++ client/src/portals/ModalPostDetail.jsx
@@ -0,0 +1,11 @@
+export function ModalPostDetail ({ post, setPost }) {
+  return (
+    <div className='absolute top-0 left-0 w-full h-screen flex justify-center items-center gap-10'>
+      <img className='max-w-sm' src={`http://localhost:3000/images/${post.photo}`} />
+      <p className='bg-slate-800 text-white' dangerouslySetInnerHTML={{ __html: post.plot }} />
+      <button className='absolute top-8 right-8 text-4xl' onClick={() => setPost(null)}>
+        X
+      </button>
+    </div>
+  )
+}
