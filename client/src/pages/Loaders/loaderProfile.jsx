d3d50fd40069a530f1b934bf0eb1ef7da8442512
DylanSuarez <2000sajo@gmail.com>
2023-04-01 17:33:23 -0500

    zustand client

--- client/src/pages/Loaders/loaderProfile.jsx
+++ client/src/pages/Loaders/loaderProfile.jsx
@@ -1,7 +1,7 @@
 import { BASE_URL } from '../../services/settings'
 
 export const loaderProfile = async ({ params }) => {
-  const user = await (await fetch(`${BASE_URL}/users/${params.username}/`)).json()
+  const user = await (await fetch(`${BASE_URL}/users/${params.username}`)).json()
   const userPosts = await (await fetch(`${BASE_URL}/users/${user.id}/posts`)).json()
 
   return { user, userPosts }
