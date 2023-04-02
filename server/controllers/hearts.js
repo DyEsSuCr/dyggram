82cd7f702a5710cfbc11163f940fd1c99fe63068
DylanSuarez <2000sajo@gmail.com>
2023-04-02 14:56:17 -0500

    deleteHearts

--- server/controllers/hearts.js
+++ server/controllers/hearts.js
@@ -12,3 +12,18 @@ export const createHeart = async (req, res) => {
     res.status(404).json(err)
   }
 }
+
+export const deleteHeart = async (req, res) => {
+  try {
+    await Heart.destroy({
+      where: {
+        userId: req.user.id,
+        postId: req.body.post
+      }
+    })
+
+    res.status(200).json({ messaje: 'Deleted heart' })
+  } catch (err) {
+    res.status(404).json(err)
+  }
+}
--- server/routes/heart.js
+++ server/routes/heart.js
@@ -5,5 +5,6 @@ import { hearts } from '../controllers/index.js'
 const router = Router()
 
 router.post('/hearts', verifyToken, hearts.createHeart)
+router.delete('/hearts', verifyToken, hearts.deleteHeart)
 
 export default router
