b6f5f72339c5406d02905ce4f878cc0af4a487c2
DylanSuarez <2000sajo@gmail.com>
2023-04-02 13:41:05 -0500

    fakeData

--- client/src/portals/ModalPostDetail.jsx
+++ client/src/portals/ModalPostDetail.jsx
@@ -1,6 +1,6 @@
 export function ModalPostDetail ({ post, setPost }) {
   return (
-    <div className='absolute top-0 left-0 w-full h-screen flex justify-center items-center gap-10'>
+    <div className='absolute top-0 left-0 w-full h-screen flex justify-center items-center gap-10 bg-[#2f2f2f50]'>
       <img className='max-w-sm' src={`http://localhost:3000/images/${post.photo}`} />
       <p className='bg-slate-800 text-white' dangerouslySetInnerHTML={{ __html: post.plot }} />
       <button className='absolute top-8 right-8 text-4xl' onClick={() => setPost(null)}>
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import app from './app.js'
 import { conectionSequelize } from './database/db.js'
+import { fakeData } from './faker/index.js'
 
 import './models/index.js'
 
 const main = async () => {
   try {
     await conectionSequelize.sync({ force: false })
+    if (+process.argv[2]) fakeData()
 
     app.listen(app.get('port'), () => {
       console.log(`🆗✅🆗 Server on port ${app.get('port')} 🆗✅🆗`)
